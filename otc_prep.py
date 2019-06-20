dd1fb5610051c1e86f57f7e21937d4f8c1af6a2b
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-20 14:53:54 -0400

    keytrain add

--- otc_prep.py
+++ otc_prep.py
@@ -1,8 +1,6 @@
 # Load libraries
 import numpy as np
 import gin.tf
-from obstacle_tower_env import ActionFlattener
-from gym.spaces.box import Box
 
 # Preprocess environment
 @gin.configurable
@@ -13,7 +11,7 @@ class Preprocessing(object):
   do anything else to the environment.
   """
 
-  def __init__(self, environment, reshape_action=False, fetch_key=False):
+  def __init__(self, environment):
     """Constructor for an Obstacle Tower preprocessor.
 
     Args:
@@ -21,21 +19,14 @@ class Preprocessing(object):
 
     """
     self.environment = environment
-    self.fetch_key = fetch_key
 
     self.game_over = False
     self.lives = 0  # Will need to be set by reset().
-
-    if reshape_action:
-      self.environment._flattener = ActionFlattener([2, 3, 2, 1])
-      self.environment._action_space = self.environment._flattener.action_space
+    self.keys = 0
 
   @property
   def observation_space(self):
-    if self.fetch_key:
-      return Box(0, 255, shape=(7064,))
-    else:
-      return self.environment.observation_space
+    return self.environment.observation_space
 
   @property
   def spec(self):
@@ -63,8 +54,7 @@ class Preprocessing(object):
   def reset(self):
     observation = self.environment.reset()
     observation = observation / 255.
-    if self.fetch_key:
-      observation = np.append(observation, [1, 0, 0, 0, 0, 0, 1, 0])
+    self.keys = 0
     return observation
 
   def render(self, mode):
@@ -74,8 +64,7 @@ class Preprocessing(object):
     observation, reward, game_over, info = self.environment.step(action)
     self.game_over = game_over
     observation = observation / 255.
-    if self.fetch_key:
-      key_obs = info['brain_info'].vector_observations
-      key_obs[-1, -2] = key_obs[-1, -2] / 3000
-      observation = np.append(observation, key_obs)
+    if info['total_keys'] > self.keys:
+      reward = reward + 3.0
+      self.keys = info['total_keys']
     return observation, reward, game_over, info
