8d4411556cf22704af5dc278fbbc3e8983b52138
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-20 15:21:04 -0400

    enable TRPO

--- stable_baselines/__init__.py
+++ stable_baselines/__init__.py
@@ -6,7 +6,7 @@ from stable_baselines.a2c import A2C
 #from stable_baselines.gail import GAIL
 #from stable_baselines.ppo1 import PPO1
 from stable_baselines.ppo2 import PPO2
-#from stable_baselines.trpo_mpi import TRPO
+from stable_baselines.trpo_mpi import TRPO
 #from stable_baselines.sac import SAC
 
 __version__ = "2.5.1"
