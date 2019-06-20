56ed7d3dbb2d613bde005e3a2ae678588a61d89e
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-20 15:26:28 -0400

    fix TRPO

--- trpo_basictrain.py
+++ trpo_basictrain.py
@@ -16,17 +16,17 @@ if __name__ == "__main__":
     parser.add_argument('--num_timesteps', type=int, default=int(3e7))
     args = parser.parse_args()
 
+    log_dir = "models/trpo/"
+    os.makedirs(log_dir, exist_ok=True)
+
     env = ObstacleTowerEnv('./ObstacleTower/obstacletower', worker_id=seed,
                            retro=True, config={'total-floors': 8}, greyscale=True, timeout_wait=600)
     env._flattener = ActionFlattener([2, 3, 2, 1])
     env._action_space = env._flattener.action_space
     env = Preprocessing(env)
-    env = Monitor(env, sub_dir)
+    env = Monitor(env, log_dir)
 
-    log_dir = "models/trpo/"
-    os.makedirs(log_dir, exist_ok=True)
 
-    # Create PPO model for GPU
     multimodel = TRPO(CnnPolicy, env, verbose=1, gamma=args.gamma)
     multimodel.learn(total_timesteps=args.num_timesteps)
     multimodel.save('models/trpo/trpomodel')
