a2b62fba852e2563e3b6481e45309ad21e4e32d4
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-21 10:28:11 -0400

    add main loop

--- rnd/train.py
+++ rnd/train.py
@@ -2,7 +2,6 @@ import numpy as np
 import os
 import re
 import glob
-import gc
 
 import torch.nn.functional as F
 import torch
@@ -362,9 +361,9 @@ def main():
             torch.save(rnd.predictor.state_dict(), incre_predictor_path)
             torch.save(rnd.target.state_dict(), incre_target_path)
             if args.terminate and (global_step > args.terminate_steps):
-                gc.collect()
                 break
 
 
 if __name__ == '__main__':
-    main()
+    for i in range(10):
+        main()
