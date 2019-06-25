74cbad2f60f3c5578d8e2cd1f2e35746510cb303
hanschoi86 <hanschoi86@gmail.com>
2019-06-25 15:03:08 +0000

    fix reward

--- rnd/otcenv/obstacle_tower_env.py
+++ rnd/otcenv/obstacle_tower_env.py
@@ -212,7 +212,7 @@ class ObstacleTowerEnv(gym.Env):
         self.game_over = done
 
         if info.get('total_keys') > self.current_keys:
-            reward = 1.21
+            reward = 1.11
             self.current_keys = info.get('total_keys')
 
         if info.get('current_floor') > self.current_floor:
