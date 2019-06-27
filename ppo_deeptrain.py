aaa079f947ed03badaff69f9feb5c0027e5c0931
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-27 14:25:14 -0400

    update deeptrain

--- ppo_deeptrain.py
+++ ppo_deeptrain.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=16)
     parser.add_argument('--gamma', help='PPO gamma', type=float, default=0.999)
     parser.add_argument('--num_timesteps', type=int, default=int(5e7))
-    parser.add_argument('--learning_rate', type=float, default=.0000625)
+    parser.add_argument('--learning_rate', type=float, default=.0003)
     args = parser.parse_args()
 
     num_cpu = args.num_cpu
