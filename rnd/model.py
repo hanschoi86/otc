a1c728f4e778ee28c7a74bac0e81d77251ac8b13
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-27 10:14:42 -0400

    update model nn

--- rnd/model.py
+++ rnd/model.py
@@ -79,15 +79,31 @@ class CnnActorCriticNetwork(nn.Module):
         else:
             linear = nn.Linear
 
+        #self.feature = nn.Sequential(
+        #    nn.Conv2d(4, 32, kernel_size=8, stride=4),
+        #    nn.ReLU(),
+        #    nn.Conv2d(32, 64, kernel_size=4, stride=2),
+        #    nn.ReLU(),
+        #    nn.Conv2d(64, 64, kernel_size=3, stride=1),
+        #    nn.ReLU(),
+        #    Flatten(),
+        #    linear(7 * 7 * 64, 256),
+        #    nn.ReLU(),
+        #    linear(256, 448),
+        #    nn.ReLU()
+        #)
+
         self.feature = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size=8, stride=4),
+            nn.Conv2d(4, 8, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             Flatten(),
-            linear(7 * 7 * 64, 256),
+            linear(10 * 10 * 64, 256),
             nn.ReLU(),
             linear(256, 448),
             nn.ReLU()
