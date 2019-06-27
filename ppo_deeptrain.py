7630c8f940c1c210e8f7fcb845914007b3f5dba3
hanchul_choi <hanchul.choi@spglobal.com>
2019-06-27 14:15:56 -0400

    deep ppo

--- ppo_deeptrain.py
+++ ppo_deeptrain.py
@@ -3,19 +3,19 @@ import os
 import argparse
 from obstacle_tower_env import ObstacleTowerEnv, ActionFlattener
 
-from stable_baselines.common.policies import MlpPolicy, CnnPolicy
-from stable_baselines.common.vec_env import SubprocVecEnv
-from stable_baselines.bench import Monitor
-from stable_baselines import PPO2
+from deep_baselines.common.policies import MlpPolicy, CnnPolicy
+from deep_baselines.common.vec_env import SubprocVecEnv
+from deep_baselines.bench import Monitor
+from deep_baselines import PPO2
 
-seed = np.random.randint(6000, 7000)
+seed = np.random.randint(0, 100)
 
 def make_env(log_dir, cpu):
     sub_dir = "{}cpu_{}/".format(log_dir, cpu)
     os.makedirs(sub_dir, exist_ok=True)
     def _init():
         env = ObstacleTowerEnv('./ObstacleTower/obstacletower', worker_id=seed+cpu,
-                               retro=True, config={'starting-floor': 4, 'total-floors': 12}, greyscale=True, timeout_wait=600)
+                               retro=True, config={'total-floors': 6}, greyscale=True, timeout_wait=600)
         env._flattener = ActionFlattener([2, 3, 2, 1])
         env._action_space = env._flattener.action_space
         env = Monitor(env, sub_dir)
@@ -24,10 +24,10 @@ def make_env(log_dir, cpu):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=12)
+    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=16)
     parser.add_argument('--gamma', help='PPO gamma', type=float, default=0.999)
-    parser.add_argument('--num_timesteps', type=int, default=int(3e7))
-    parser.add_argument('--learning_rate', type=float, default=.000135)
+    parser.add_argument('--num_timesteps', type=int, default=int(5e7))
+    parser.add_argument('--learning_rate', type=float, default=.0000625)
     args = parser.parse_args()
 
     num_cpu = args.num_cpu
@@ -37,6 +37,6 @@ if __name__ == "__main__":
     multienv = SubprocVecEnv([make_env(log_dir, cpu) for cpu in range(num_cpu)])
 
     # Create PPO model for GPU
-    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate)
+    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate, n_steps=256)
     multimodel.learn(total_timesteps=args.num_timesteps)
     multimodel.save('models/ppodeep/deepmodel')
