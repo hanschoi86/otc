e5f10d5171e68106fd714ba4d0aeb8bf796b6f8a
hanschoi86 <hanschoi86@gmail.com>
2019-06-28 16:31:50 +0000

    deeptrain

--- ppo_deeptrain.py
+++ ppo_deeptrain.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=16)
     parser.add_argument('--gamma', help='PPO gamma', type=float, default=0.999)
-    parser.add_argument('--num_timesteps', type=int, default=int(5e7))
+    parser.add_argument('--num_timesteps', type=int, default=int(3e7))
     parser.add_argument('--learning_rate', type=float, default=.0003)
     args = parser.parse_args()
 
