0a5807f58d3e3b87ee38e28b571f3633049ea2cf
hanschoi86 <hanschoi86@gmail.com>
2019-06-29 10:39:09 -0400

    deeptrain2

--- ppo_deeptrain2.py
+++ ppo_deeptrain2.py
@@ -3,10 +3,10 @@ import os
 import argparse
 from obstacle_tower_env import ObstacleTowerEnv, ActionFlattener
 
-from stable_baselines.common.policies import CnnPolicy
-from stable_baselines.common.vec_env import SubprocVecEnv
+from deep_baselines.common.policies import MlpPolicy, CnnPolicy
+from deep_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.bench import Monitor
-from stable_baselines import PPO2
+from deep_baselines import PPO2
 
 seed = np.random.randint(0, 100)
 
@@ -24,21 +24,19 @@ def make_env(log_dir, cpu):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=12)
+    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=16)
     parser.add_argument('--gamma', help='PPO gamma', type=float, default=0.999)
-    parser.add_argument('--num_timesteps', type=int, default=int(15000000))
-    parser.add_argument('--learning_rate', type=float, default=.000015)
+    parser.add_argument('--num_timesteps', type=int, default=int(3e7))
+    parser.add_argument('--learning_rate', type=float, default=.0002)
     args = parser.parse_args()
 
     num_cpu = args.num_cpu
-    log_dir = "models/ppo/"
+    log_dir = "models/ppodeep/"
     os.makedirs(log_dir, exist_ok=True)
     # Create vectorized multi-environment to run on 12 cores
     multienv = SubprocVecEnv([make_env(log_dir, cpu) for cpu in range(num_cpu)])
 
     # Create PPO model for GPU
-    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate, 
-                      cliprange=.1, n_steps=256)
-    multimodel = multimodel.load('models/ppo/multimodel7', multienv)
+    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate, n_steps=256)
     multimodel.learn(total_timesteps=args.num_timesteps)
-    multimodel.save('models/ppo/multimodel8')
+    multimodel.save('models/ppodeep/deepmodel')
