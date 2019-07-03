aa7c2a14c41e5f2025ef5bd37d6dfa882cd6f829
hanchul_choi <hanchul.choi@spglobal.com>
2019-07-03 09:09:10 -0400

    runhigh model

--- ppo_runninghigh.py
+++ ppo_runninghigh.py
@@ -3,10 +3,12 @@ import os
 import argparse
 from obstacle_tower_env import ObstacleTowerEnv, ActionFlattener
 
-from stable_baselines.common.policies import MlpPolicy, CnnPolicy
+from stable_baselines.common.policies import CnnPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.bench import Monitor
 from stable_baselines import PPO2
+from otc_prep import Preprocessing
+
 
 seed = np.random.randint(0, 100)
 
@@ -15,19 +17,20 @@ def make_env(log_dir, cpu):
     os.makedirs(sub_dir, exist_ok=True)
     def _init():
         env = ObstacleTowerEnv('./ObstacleTower/obstacletower', worker_id=seed+cpu,
-                               retro=True, config={'total-floors': 8}, greyscale=True, timeout_wait=600)
+                               retro=False, config={'total-floors': 10}, greyscale=True, timeout_wait=600)
         env._flattener = ActionFlattener([2, 3, 2, 1])
         env._action_space = env._flattener.action_space
+        env = Preprocessing(env)
         env = Monitor(env, sub_dir)
         return env
     return _init
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=12)
+    parser.add_argument('--num_cpu', help='number of cpu cores', type=int, default=16)
     parser.add_argument('--gamma', help='PPO gamma', type=float, default=0.999)
-    parser.add_argument('--num_timesteps', type=int, default=int(1e8))
-    parser.add_argument('--learning_rate', type=float, default=.0000625)
+    parser.add_argument('--num_timesteps', type=int, default=int(1e7))
+    parser.add_argument('--learning_rate', type=float, default=.00003)
     args = parser.parse_args()
 
     num_cpu = args.num_cpu
@@ -37,6 +40,8 @@ if __name__ == "__main__":
     multienv = SubprocVecEnv([make_env(log_dir, cpu) for cpu in range(num_cpu)])
 
     # Create PPO model for GPU
-    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate)
+    multimodel = PPO2(CnnPolicy, multienv, verbose=1, gamma=args.gamma, learning_rate=args.learning_rate, 
+                      cliprange=.1, n_steps=128)
+    multimodel = multimodel.load('models/ppo/multimodel_running', multienv)
     multimodel.learn(total_timesteps=args.num_timesteps)
-    multimodel.save('models/ppo/multimodel')
+    multimodel.save('models/ppo/multimodel_runninghigh')
